0318ed457c0047e39b610f0ec8a79252e2bca228
HEITIM <t.hein@ctisystems.com>
2023-11-23 16:20:11 +0100

    126

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  showAlert = false;
+  alertMsg = 'Please wait! You are being logged in.'
+  alertColor = 'blue'
+
+  credentials = { 
+    email: '',
+    password: ''
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {
+    
+  }
+
+  login() {
+    this.showAlert = true
+    this.alertMsg = 'Please wait! You are being logged in.'
+    this.alertColor = 'blue'
+  }
 }
