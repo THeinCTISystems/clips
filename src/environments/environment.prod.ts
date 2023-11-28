cc17cb8b78e26a833e6b465b6c711aee1fe4d8be
HEITIM <t.hein@ctisystems.com>
2023-11-28 15:52:34 +0100

    153

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyBN-HSP2h8g7bqAMwt_J1KI-zslxYectSY",
+        authDomain: "clips-f1842.firebaseapp.com",
+        projectId: "clips-f1842",
+        storageBucket: "clips-f1842.appspot.com",
+        messagingSenderId: "304435818634",
+        appId: "1:304435818634:web:72908ba155d282fdf1bd73"
+      }
+};
