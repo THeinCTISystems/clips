999b8b0fd63586e7c7b53a1108d6d5546fb2727c
HEITIM <t.hein@ctisystems.com>
2023-11-29 14:09:57 +0100

    155

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-register',
@@ -7,6 +8,9 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+
+  constructor(private auth: AngularFireAuth) {}
+
   name = new FormControl('', [
     Validators.required,
     Validators.minLength(3)
@@ -46,9 +50,27 @@ export class RegisterComponent {
     phoneNumber: this.phoneNumber
   })
 
-  register() {
+  async register() {
     this.showAlert = true
     this.alertMsg = 'Please wait! Your account is being created.'
     this.alertColor = 'blue'
+    
+    const {email, password} = this.registerForm.value;
+
+    try {
+      const userCred = await this.auth.createUserWithEmailAndPassword (
+        email as string, password as string
+      )
+      console.log(userCred) 
+    }catch(e){
+      console.error(e)
+
+      this.alertMsg = 'An unexpected error occured. Please try again later.'
+      this.alertColor = 'red'
+      return
+    }
+
+    this.alertMsg = 'Success! Your account has been created.'
+    this.alertColor = 'green'
   }
 }
