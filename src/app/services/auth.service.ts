597a1dfb737bbc0b57d963c8a28f5a04ea8df22d
HEITIM <t.hein@ctisystems.com>
2023-12-04 09:30:12 +0100

    165

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,18 +2,23 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 import IUser from '../models/user.model';
+import { Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private usersCollection: AngularFirestoreCollection<IUser>
+  public isAuthenticated$: Observable<boolean>
 
   constructor (
     private auth: AngularFireAuth,
     private db: AngularFirestore  
   ) { 
     this.usersCollection = db.collection('users')
+    this.isAuthenticated$ = auth.user.pipe(
+      map(user => !!user)
+    )
   }
 
   public async createUser(userData: IUser) {
@@ -25,11 +30,19 @@ export class AuthService {
       userData.email as string, userData.password as string
     )
     
-    await this.usersCollection.add({
+    if (!userCred.user){
+      throw new Error("User can't be found!")
+    }
+
+    await this.usersCollection.doc(userCred.user?.uid).set({
       name: userData.name, 
       email: userData.email,
       age: userData.age,
       phoneNumber: userData.phoneNumber
+    })
+
+    await userCred.user.updateProfile({
+      displayName: userData.name
     })
   }
 }
