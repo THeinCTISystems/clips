fb923c878fbca8feb5368f8b11699e8c96b346e6
HEITIM <t.hein@ctisystems.com>
2024-01-29 11:21:14 +0100

    283

--- src/app/clips-list/clips-list.component.ts
+++ src/app/clips-list/clips-list.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ClipService } from '../services/clip.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-clips-list',
   templateUrl: './clips-list.component.html',
-  styleUrls: ['./clips-list.component.css']
+  styleUrls: ['./clips-list.component.css'],
+  providers: [DatePipe]
 })
 export class ClipsListComponent implements OnInit, OnDestroy{
 
-  constructor() { }
+  constructor(public clipService: ClipService) {
+    this.clipService.getClips()
+   }
 
   ngOnInit(): void {
     window.addEventListener('scroll', this.handleScroll)
@@ -24,7 +29,7 @@ export class ClipsListComponent implements OnInit, OnDestroy{
     const bottomOfWindow = Math.round(scrollTop) + innerHeight === offsetHeight
 
     if (bottomOfWindow) {
-      console.log('bottom of window')
+      this.clipService.getClips()
     }
   }
 
